3c078bb81dfe392613943afe66900bab9a3c8837
haeunsong <jamy022576@gmail.com>
2025-02-10 00:09:28 +0900

    Feat : 스프링 프로젝트 생성

---
+++ src/test/java/com/mentos/mentosback/MentosBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentos.mentosback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentosBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
