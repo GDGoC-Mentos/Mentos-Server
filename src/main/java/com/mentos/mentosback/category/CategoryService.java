ecd800e6fb40c480b4d71c63bf28440c561d3c31
hyejin <hyejin66@sookmyung.ac.kr>
2025-02-14 01:35:28 +0900

    feat: 사용자, 카테고리 할 일과 연결 (#11)

--- src/main/java/com/mentos/mentosback/category/CategoryService.java
+++ src/main/java/com/mentos/mentosback/category/CategoryService.java
@@ -1,5 +1,7 @@
 package com.mentos.mentosback.category;
 
+import com.mentos.mentosback.common.apiPayload.code.status.ErrorStatus;
+import com.mentos.mentosback.common.apiPayload.exception.GeneralException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,8 @@ public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
+    public Category findById(Long categoryId) {
+        return categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new GeneralException(ErrorStatus.ID_NOT_FOUND));
+    }
 }
